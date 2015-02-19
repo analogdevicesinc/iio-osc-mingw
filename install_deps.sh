3b5f46a357cd2bf8d79931bdf3f5fc3dc272b916
Paul Cercueil <paul.cercueil@analog.com>
2015-02-19 16:37:54 +0100

    Update list of dependencies
    Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

--- install_deps.sh
+++ install_deps.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-apt-get install oasis indent gcc-mingw-w64-i686 gcc-mingw-w64-x86_64 cmake
+apt-get install cmake autoconf automake libtool \
+	libglib2.0-dev libgtk2.0-dev oasis indent \
+	gcc-mingw-w64-i686 gcc-mingw-w64-x86_64 \
+	transfig texinfo
