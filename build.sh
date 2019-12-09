9d9743074acc6b8967c4556374f85ad1281f360e
Dan Nechita <dan.nechita@analog.com>
2019-12-09 16:38:51 +0200

    appveyor: Split 32-bit and 64-bit builds into two jobs
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+if [[ -z "$1" ]]; then
+  echo "Provide the MinGW version (i686 or x86_64)"
+  exit
+fi
+
 CURDIR=`realpath "$0"`
 export CURDIR=`dirname "${CURDIR}"`
 
 [ -z "$BRANCH" ] && BRANCH="master"
 
-export MINGW_VERSION=i686
-cd ${CURDIR}
-. ./build.sh.inc
-
-export MINGW_VERSION=x86_64
+export MINGW_VERSION=$1
 cd ${CURDIR}
 . ./build.sh.inc
 
